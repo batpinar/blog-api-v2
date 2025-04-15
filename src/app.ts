c8d7b07aea3f6c2acf54014ced9b2d230b08a145
batpinar <batpnar@gmail.com>
2025-04-15 21:29:03 +0300

    add tag model, routes and controller

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@ import express from 'express';
 import dotenv from  'dotenv';
 import categoryRoutes from './routes/categoryRoutes.js'
 import postRoutes from './routes/postRoutes.js'
- import commentRoutes from './routes/commentRoutes.js'
+import commentRoutes from './routes/commentRoutes.js'
+import tagRoutes from './routes/tagRoutes.js'
 
 dotenv.config();
 
@@ -11,7 +12,8 @@ app.use(express.json());
 
 app.use('/api/categories', categoryRoutes);
 app.use('/api/posts', postRoutes);
- app.use('/api/comments', commentRoutes)
+app.use('/api/comments', commentRoutes)
+app.use('/api/tags', tagRoutes)
 
 const PORT = process.env.PORT
 app.listen(PORT || 3001, () =>{
